85c83426f97bb0dc5323e0f583b8cade4d0aeeeb
Rafael Kenji Nissi <rafael.nissi@usp.br>
2022-11-14 10:10:44 -0300

    Added some explanations in some files

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 FROM rabbitmq:3.10-management
 
+#Copies a file from the host filesystem to the image internal filesystem
 ADD init.sh /init.sh
 
+#tells that the container listen to a certain port
 EXPOSE 5672
+
+#Run a shell command
 RUN ["chmod", "+x", "/init.sh"]
+
+#Configure the container that will run as a executable
 ENTRYPOINT ["/init.sh"]
 
--- init.sh
+++ init.sh
@@ -10,7 +10,13 @@ rabbitmqctl set_permissions -p / admin ".*" ".*" ".*" ; \
 
 rabbitmq-plugins enable rabbitmq_management ; \
 
+rabbitmqadmin declare exchange --vhost=/ name=personCreationFanoutExchange type=fanout ; \
+
 rabbitmqadmin declare exchange --vhost=/ name=personCreationTopicExchange type=topic ; \
+
+rabbitmqadmin declare exchange --vhost=/ name=personCreationDirectExchange type=direct ; \
+
+rabbitmqadmin declare exchange --vhost=/ name=personCreationHeaderExchange type=headers; \
 
 rabbitmqadmin declare exchange --vhost=/ name=personCreationTopicDLQExchange type=fanout ; \
 
@@ -20,8 +26,19 @@ rabbitmqadmin --vhost=/ declare binding source="personCreationTopicDLQExchange" 
 
 rabbitmqadmin declare queue --vhost=/ name=personCreationQueue durable=true arguments={\"x-dead-letter-exchange\":\"personCreationTopicDLQExchange\",\"x-dead-letter-routing-key\":\"personCreationTopicDLQExchange\"} ; \
 
-rabbitmqadmin --vhost=/ declare binding source="personCreationTopicExchange" destination_type="queue" destination="personCreationQueue" routing_key="create.*" ; \
+rabbitmqadmin declare queue --vhost=/ name=personCreationQueueAllHeaders durable=true ; \
 
+rabbitmqadmin --vhost=/ declare binding source="personCreationTopicExchange" destination_type="queue" destination="personCreationQueue" routing_key="topic.*" ; \
+
+rabbitmqadmin --vhost=/ declare binding source="personCreationDirectExchange" destination_type="queue" destination="personCreationQueue" routing_key="direct.key" ; \
+
+rabbitmqadmin --vhost=/ declare binding source="personCreationFanoutExchange" destination_type="queue" destination="personCreationQueue" routing_key="fanout.key" ; \
+
+rabbitmqadmin --vhost=/ declare binding source="personCreationHeaderExchange" destination_type="queue" destination="personCreationQueue" routing_key="" arguments={\"x-match\":\"any\",\"exchangeHeader1\":\"exchangeKey1\",\"exchangeHeader2\":\"exchangeKey2\"}; \
+#rabbitmqadmin --vhost=/ declare binding source="personCreationHeaderExchange" destination_type="queue" destination="personCreationQueue" routing_key="" arguments={\"x-match\":\"any-with-x\",\"testHeader\":\"TEST\",\"testHeader2\":\"TEST2\"}; \
+
+rabbitmqadmin --vhost=/ declare binding source="personCreationHeaderExchange" destination_type="queue" destination="personCreationQueueAllHeaders" routing_key="" arguments={\"x-match\":\"all\",\"exchangeHeader1\":\"exchangeKey1\",\"exchangeHeader2\":\"exchangeKey2\"} ; \
+#rabbitmqadmin --vhost=/ declare binding source="personCreationHeaderExchange" destination_type="queue" destination="personCreationQueueAllHeaders" routing_key="" arguments={\"x-match\":\"all-with-x\",\"testHeader\":\"TEST\",\"testHeader2\":\"TEST2\"} ; \
 
 
 )&
